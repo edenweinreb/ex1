41e861eee86ef55150c23863ee55878076ff83ae
Shiraz Oskar <shiraz.osc@gmail.com>
2026-05-03 14:20:41 +0300

    Implement FileRepository class (inheriting from the interface).

---
+++ Ex1/src/FileRepository.h
@@ -0,0 +1,24 @@
+#ifndef FILE_REPOSITORY_H
+#define FILE_REPOSITORY_H
+
+#include "IDataRepository.h"
+#include <string>
+#include <vector>
+#include <map>
+
+class FileRepository : public IDataRepository {
+private:
+    std::string filePath;
+    std::map<int, User> userData;
+
+public:
+    FileRepository(const std::string& path);
+
+    void addViewedProduct(int userId, const Product& product) override;
+    std::vector<Product> getUserData(int userId) override;
+    void loadAll() override;
+
+    virtual ~FileRepository() {}
+}
+
+#endif
