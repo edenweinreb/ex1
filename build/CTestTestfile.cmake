785d5b109c35271f65272654817a28e2bf22aaa4
Zohar <zohar.lidani@live.biu.ac.il>
2026-05-05 18:40:54 +0300

    Fix git config files and remove build folder from tracking

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/cs - study/year 2/Advenced Programing/REX1/ex1/Ex1
-# Build directory: C:/cs - study/year 2/Advenced Programing/REX1/ex1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/cs - study/year 2/Advenced Programing/REX1/ex1/build/run_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
