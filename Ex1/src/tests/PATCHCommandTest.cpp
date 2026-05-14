cc14566a6d17fb3827547ec09fb338debcf7f0ed
Shiraz Oskar <shiraz.oskar@live.biu.ac.il>
2026-05-14 22:41:10 +0300

    patch test

---
+++ Ex1/src/tests/PATCHCommandTest.cpp
@@ -0,0 +1,125 @@
+#include <gtest/gtest.h>
+#include "CommandParser.h"
+#include "FileRepository.h"
+#include "ICommand.h"
+#include <fstream>
+
+// helper to create a repo for testing
+class PATCHCommandTest : public ::testing::Test {
+protected:
+    FileRepository repo{"data/test.csv"};
+    void SetUp() override {
+        // Empty the file contents before each test starts
+        std::ofstream ofs("data/test.csv", std::ofstream::trunc);
+    }
+};
+
+// "404 Not Found"
+// "204 No Content"
+
+TEST_F(PATCHCommandTest, ParseValidPATCHCommand) {
+    ICommand* cmd1 = CommandParser::parse("POST 20 400", repo);
+    ASSERT_NE(cmd1, nullptr);
+    EXPECT_EQ(cmd1->execute(), "201 Created");
+    delete cmd1; 
+
+    ICommand* cmd2 = CommandParser::parse("PATCH 20 500", repo); 
+    ASSERT_NE(cmd2, nullptr);
+    
+    EXPECT_EQ(cmd2->execute(), "204 No Content"); 
+    delete cmd2;
+
+    std::set<int> userProducts = repo.getUserData(20);
+    std::set<int> productUsers = repo.getProductUsers(500);
+    
+    EXPECT_EQ(userProducts.size(), 2);
+    EXPECT_TRUE(userProducts.count(400));
+    EXPECT_TRUE(userProducts.count(500));
+    EXPECT_EQ(productUsers.size(), 1);
+    EXPECT_TRUE(productUsers.count(20));
+}
+
+TEST_F(PATCHCommandTest, ParseSamePIdPATCHCommand) {
+    ICommand* cmd1 = CommandParser::parse("POST 20 400", repo);
+    ASSERT_NE(cmd1, nullptr);
+    EXPECT_EQ(cmd1->execute(), "201 Created");
+    delete cmd1; 
+
+    ICommand* cmd2 = CommandParser::parse("PATCH 20 500 500", repo); 
+    ASSERT_NE(cmd2, nullptr);
+    
+    EXPECT_EQ(cmd2->execute(), "204 No Content"); 
+    delete cmd2;
+
+    std::set<int> productUsers = repo.getProductUsers(400);
+    std::set<int> userProducts = repo.getUserData(20);
+    EXPECT_EQ(userProducts.size(), 2);
+    EXPECT_TRUE(userProducts.count(400));
+    EXPECT_TRUE(userProducts.count(500));
+    EXPECT_EQ(productUsers.size(), 1);
+    EXPECT_TRUE(productUsers.count(20));
+}
+
+TEST_F(PATCHCommandTest, PATCHCommandWithExtraSpaces) {
+    ICommand* cmd1 = CommandParser::parse("POST 20 400", repo);
+    ASSERT_NE(cmd1, nullptr);
+    EXPECT_EQ(cmd1->execute(), "201 Created");
+    delete cmd1; 
+
+    ICommand* cmd2 = CommandParser::parse("  PATCH   20   500  ", repo); 
+    ASSERT_NE(cmd2, nullptr);
+    
+    EXPECT_EQ(cmd2->execute(), "204 No Content"); 
+    delete cmd2;
+
+    std::set<int> productUsers = repo.getProductUsers(400);
+    std::set<int> userProducts = repo.getUserData(20);
+    EXPECT_EQ(userProducts.size(), 1);
+    EXPECT_TRUE(userProducts.count(400));
+    EXPECT_TRUE(userProducts.count(500));
+    EXPECT_EQ(productUsers.size(), 1);
+    EXPECT_TRUE(productUsers.count(20));
+}
+
+TEST_F(PATCHCommandTest, PATCHFailsIfUserDoNotExists) {
+    ICommand* cmd = CommandParser::parse("PATCH 20 400", repo);
+    ASSERT_NE(cmd, nullptr);
+    EXPECT_EQ(cmd->execute(), "404 Not Found");
+    delete cmd;
+}
+
+TEST_F(PATCHCommandTest, InvalidFormatMissingProductId) {
+    ICommand* cmd1 = CommandParser::parse("POST 20 400", repo);
+    ASSERT_NE(cmd1, nullptr);
+    EXPECT_EQ(cmd1->execute(), "201 Created");
+    delete cmd1; 
+
+    ICommand* cmd2 = CommandParser::parse("PATCH 20", repo); 
+    ASSERT_NE(cmd2, nullptr);
+    EXPECT_EQ(cmd2->execute(), "400 Bad Request"); 
+    delete cmd2;
+}
+
+TEST_F(PATCHCommandTest, InvalidNonNumericInput) {
+    ICommand* cmd1 = CommandParser::parse("POST 20 400", repo);
+    ASSERT_NE(cmd1, nullptr);
+    EXPECT_EQ(cmd1->execute(), "201 Created");
+    delete cmd1; 
+
+    ICommand* cmd2 = CommandParser::parse("PATCH aaa 400", repo); 
+    ASSERT_NE(cmd2, nullptr);
+    EXPECT_EQ(cmd2->execute(), "400 Bad Request"); 
+    delete cmd2;
+}
+
+TEST_F(PATCHCommandTest, InvalidNonNumericProductInput) {
+    ICommand* cmd1 = CommandParser::parse("POST 20 400", repo);
+    ASSERT_NE(cmd1, nullptr);
+    EXPECT_EQ(cmd1->execute(), "201 Created");
+    delete cmd1; 
+
+    ICommand* cmd2 = CommandParser::parse("PATCH 20 aaa", repo); 
+    ASSERT_NE(cmd2, nullptr);
+    EXPECT_EQ(cmd2->execute(), "400 Bad Request"); 
+    delete cmd2;
+}
