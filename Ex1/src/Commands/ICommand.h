268ddba8c55207e13223fb139555e212a95e9630
Shiraz Oskar <shiraz.oskar@live.biu.ac.il>
2026-05-17 21:24:26 +0300

    build

--- Ex1/src/Commands/ICommand.h
+++ Ex1/src/Commands/ICommand.h
@@ -1,22 +1,12 @@
-#pragma once
+#ifndef ICOMMAND_H
+#define ICOMMAND_H
+
 #include <string>
-#include "CommandParser.h"
 
 class ICommand {
-protected:
-    // Stores the parameters and data for the current command
-    Command currentCmd;
-
 public:
-    // Virtual destructor ensures proper cleanup of derived classes
-    virtual ~ICommand() = default; 
-    
-    // Setter for injecting arguments into the command before execution
-    virtual void setArgs(const Command& cmd) {
-        currentCmd = cmd;
-    }
-    
-    // Executes the command's logic and returns the resulting output as a string.
-    // support sending the response over a TCP socket.
+    virtual ~ICommand() = default;
     virtual std::string execute() = 0;
 };
+
+#endif
