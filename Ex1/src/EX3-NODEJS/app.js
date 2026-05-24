d1432ecafcf985dcef88f76eadd3cd37b822a646
Shiraz Oskar <shiraz.oskar@live.biu.ac.il>
2026-05-25 00:49:08 +0300

    AE-120 Internal search engine

--- Ex1/src/EX3-NODEJS/app.js
+++ Ex1/src/EX3-NODEJS/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const orderRoutes = require('./routes/orderRoutes');
+const searchRoutes = require('./routes/searchRoutes');
+
 // Enable the server to parse incoming JSON requests
 app.use(express.json());
+
+app.use('/api', orderRoutes);
+app.use('/api', searchRoutes);
 
 // Temporary test route to verify the server is working
 app.get('/api/ping', (req, res) => {
