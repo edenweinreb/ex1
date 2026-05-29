068db59b05d664e7dcf908bc35fd2498b62d36d2
Shiraz Oskar <shiraz.oskar@live.biu.ac.il>
2026-05-29 11:16:02 +0300

    Fixing repository structure and adding project files

--- Ex1/NODEJS/routes/searchRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { search } = require('../controllers/searchController');
-
-// GET http://foo.com/api/search/:query
-router.get('/search/:query', search);
-
-module.exports = router;
--- Ex1/src/EX2-CPP/Data/DefaultIO.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-class DefaultIO {
-public:
-    virtual std::string read() = 0;
-    virtual void write(const std::string& text) = 0;
-    virtual ~DefaultIO() = default;
-};
