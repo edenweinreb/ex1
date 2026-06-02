f223dc4234370187df161a994abbcb325efde783
Shiraz Oskar <shiraz.oskar@live.biu.ac.il>
2026-06-02 21:06:37 +0300

    clean headers

--- Ex1/src/EX3-NODEJS/app.js
+++ Ex1/src/EX3-NODEJS/app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.disable('etag');
+app.use((req, res, next) => {
+  res.removeHeader('Date');
+  next();
+});
+
 // Middleware to parse JSON bodies
 app.use(express.json());
 const orderRoutes = require('./routes/orderRoutes');
